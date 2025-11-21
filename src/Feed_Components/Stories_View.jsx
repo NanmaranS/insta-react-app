a4e474898814bfdc3599e8bada3bce7e60edb5ea
Nanmaran <nanamran.s13@gmail.com.>
2025-11-21 17:54:35 +0530

    Update Stories_View component with navigation fixes

--- src/Feed_Components/Stories_View.jsx
+++ src/Feed_Components/Stories_View.jsx
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react"
-import { useParams,Link, useNavigate } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
 
-export  function Stories_View() {
-  const [story,setStory]=useState(null)
-  const {id,total}=useParams()
-const nav=useNavigate()
-  useEffect(()=>{
-fetch(`http://localhost:3000/story/${id}`)
-.then((res)=>res.json())
-.then((json_data)=>setStory(json_data))
-.catch((err)=>console.log(err));
-  },[id])
+export function Stories_View() {
+  const [story, setStory] = useState(null)
+  const { id, total } = useParams()
+  const nav = useNavigate()
 
-  if(id>total || id<=0 ){
-nav('/')
-  }
+  // Navigate home if id is invalid
+  useEffect(() => {
+    if (Number(id) <= 0 || Number(id) > Number(total)) {
+      nav('/')
+    }
+  }, [id, total, nav])
+
+  // Fetch story data
+  useEffect(() => {
+    fetch(`http://localhost:3000/story/${id}`)
+      .then(res => res.json())
+      .then(json_data => setStory(json_data))
+      .catch(err => console.log(err))
+  }, [id])
+
+  if (!story) return <div>Story Loading..</div>
+
   return (
-    <div >
-      {story ? (<div  className="d-flex justify-content-center align-items-center">
-       <Link to={`http://localhost:5173/story/${ Number(id)-1}/${total}`}><i  className="bi bi-arrow-left-circle-fill"></i></Link> 
-        <img className="vh-100" src={`/${story.image}`}/>
-     <Link to={`http://localhost:5173/story/${Number(id)+1}/${total}`} ><i className="bi bi-arrow-right-circle-fill"></i></Link> 
+    <div className="d-flex justify-content-center align-items-center">
+      {/* Previous story / home if first */}
+      <Link to={Number(id) > 1 ? `/story/${Number(id) - 1}/${total}` : `/`}>
+        <i className="bi bi-arrow-left-circle-fill"></i>
+      </Link>
 
-         </div> ): <div> Story Loading..</div>}
+      {/* Story image */}
+      <img className="vh-100 mx-3" src={`/${story.image}`} alt="Story" />
 
-
+      {/* Next story / home if last */}
+      <Link to={Number(id) < Number(total) ? `/story/${Number(id) + 1}/${total}` : `/`}>
+        <i className="bi bi-arrow-right-circle-fill"></i>
+      </Link>
     </div>
   )
 }
